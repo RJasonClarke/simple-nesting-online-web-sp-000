6913a616125a171b94aed36425be7d5c36ecc640
Robert Clarke <r.jason.clarke@gmail.com>
2020-11-11 10:59:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,9 +16,9 @@ def hopper
         }
      }
 
-end
+puts grace_hopper
 
-return :grace_hopper
+end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
